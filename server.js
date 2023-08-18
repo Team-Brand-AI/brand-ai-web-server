240595c82172513e39f6b75bb9d1b420e971b563
toothlessdev <toothless042@gmail.com>
2023-08-18 21:29:42 +0900

    80 port 귀신들림

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(cors());
 app.use("/dai/image", imageRoute);
 
 app.get("/", (request, response) => {
-    console.log(`[GET] PATH : / `);
     response.sendFile(path.join(__dirname, "public/index.html"));
 });
 
